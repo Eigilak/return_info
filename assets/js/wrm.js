8ae7aa33332ab2f76d16fa4687c6f152c431646b
Eigilak <hello@eigilak.dk>
2020-04-28 23:18:10 +0200

    Opdatering i template

--- assets/js/wrm.js
+++ assets/js/wrm.js
@@ -5,14 +5,19 @@ var app = new Vue({
    el:'.woocommerce_return_manager',
    data:{
       return_email:'',
-      return_order_id:''
+      return_order_id:'',
+      order_number:''
    },
    methods:{
+      go_to_shipmondo: function () {
+         openReturnportal('https://return.shipmondo.com/pureleaf_dk?name=hans&reference=16000&?');
+      }
 
    }
 
 
 });
+
 
 
 jQuery(document).ready(function($){
--- templates/return_order_template.php
+++ templates/return_order_template.php
@@ -1,7 +1,7 @@
 
 <?php
 
-$order = wc_get_order(17050);
+$order = wc_get_order(16950);
 
 
 ?>
@@ -10,7 +10,9 @@ $order = wc_get_order(17050);
 <div class="woocommerce_return_manager">
     <div class="wrm_overlay wrm_app">
         <div class="content">
-            <h3><?php echo get_the_title() ?> </h3>
+            <h3><?=  get_the_title() ?> - <?php _e('Return your order','wrm')  ?></h3>
+
+
             <form class="return_step_1" action="" @submit.prevent="" >
                 <div class="input">
                     <div>
@@ -24,60 +26,63 @@ $order = wc_get_order(17050);
                 <button type="submit"><?php _e('Find my order','wrm'); ?></button>
             </form>
 
-            <form class="return_step_2" action="" @submit.prevent="" >
+            <form class="return_step_2 hide" action="" @submit.prevent="" >
+                <p><?php _e('Order number') ?>: {{order_number}}</p>
                 <div class="input">
                     <table class="order_item">
                         <!--Head-->
                         <thead>
                         <tr>
-                            <th><?php _e('Return','wrm') ?></th>
+                            <th><?php _e('Choose product','wrm') ?></th>
                             <th><?php _e('Product name','wrm')?></th>
                             <th><?php _e('Return Type','wrm') ?></th>
                             <th><?php _e('Return cause','wrm') ?></th>
                             <th><?php _e('Return action','wrm') ?></th>
                         </tr>
                         </thead>
-                    <?php foreach ( $order->get_items() as $item_id => $item ) : ?>
-                        <?php
-                            /*Get all of the info for the order_item*/
-                            $product = $item->get_product();
-                            $product_id = $item->get_product_id();
-                            /*Hent billede*/
-                        ?>
                         <tbody>
-                            <tr>
-                                <td>
-                                    <input id="return_checkBox" type="checkbox" >
-                                </td>
-                                <td>
-                                    <a href="<?= get_permalink( $product_id ) ?> " target="_blank">
-                                        <?= $item->get_name() ?>
-                                    </a>
+                            <?php foreach ( $order->get_items() as $item_id => $item ) : ?>
+                                <?php
+                                    /*Get all of the info for the order_item*/
+                                    $product = $item->get_product();
+                                    $product_id = $item->get_product_id();
+                                    /*Hent billede*/
+                                ?>
 
-                                </td>
-                                <td>
-                                    <select name="" id="">
-                                        <option data-default=""><?php _e('Choose reason to return','wrm'); ?></option>
-                                        <option value="<?php _e('Damaged','wrm') ?>"><?php _e('Damaged','wrm') ?></option>
-                                        <option value="<?php _e('Wrong size','wrm') ?>"><?php _e('Wrong size','wrm') ?></option>
-                                    </select>
-                                </td>
-                                <td>
-                                    <textarea placeholder="<?php _e('Explain why you want to return','wrm') ?>"></textarea>
-                                </td>
+                                    <tr>
+                                        <td>
+                                            <input id="return_checkbox-<?= $item->get_product_id() ?>" type="checkbox" >
+                                        </td>
+                                        <td>
+                                            <label for="return_checkbox-<?= $item->get_product_id() ?>">
+                                                <?= $item->get_name() ?>
+                                            </label>
+                                        </td>
+                                        <td>
+                                            <select name="" id="">
+                                                <option data-default=""><?php _e('Choose reason to return','wrm'); ?></option>
+                                                <option value="<?php _e('Damaged','wrm') ?>"><?php _e('Damaged','wrm') ?></option>
+                                                <option value="<?php _e('Wrong size','wrm') ?>"><?php _e('Wrong size','wrm') ?></option>
+                                            </select>
+                                        </td>
+                                        <td>
+                                            <textarea placeholder="<?php _e('Explain why you want to return','wrm') ?>"></textarea>
+                                        </td>
 
-                                <td>
-                                    <textarea placeholder="<?php _e('Explain why you want to return','wrm') ?>"></textarea>
-                                </td>
-                            </tr>
+                                        <td>
+                                            <textarea id="text" placeholder="<?php _e('Explain why you want to return','wrm') ?>"></textarea>
+                                        </td>
+                                    </tr>
+                            <?php endforeach ?>
                         </tbody>
-                    <?php endforeach ?>
                     </table>
                 </div>
                 <button type="submit"><?php _e('Find my order','wrm'); ?></button>
             </form>
 
         </div>
+
+        <button v-on:click="go_to_shipmondo()">asd</button>
     </div>
 </div>
-
+<script charset="UTF-8" src="https://plugins.pakkelabels.dk/returnportal.js" type="text/javascript"></script>
